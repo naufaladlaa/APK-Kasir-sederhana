b24700097f6539b94da71789e4817713368ed290
Naufalladlaa <adlaanaufall@gmail.com>
2025-11-26 10:37:28 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 barang = {
-    "americano": 35000,
-    "dimsum": 2500,
-    "risol Mayo":2500,
-    "mochi": 15000,
-    "smothies": 15000
+    "1":{"nama" : "AMERICANO", "harga": 35000},
+    "2":{"nama" : "MOCHI", "harga": 15000},
+    "3":{"nama" : "BEEF BURGER", "harga": 40000},
+    "4":{"nama" : "KEBAB TURKEY", "harga": 40000},
+    "5":{"nama" : "MIE ACEH", "harga": 25000}
 }
